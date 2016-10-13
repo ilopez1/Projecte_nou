95394e130011475782fbbc284bf59252e67b489b
ilopez1 <ivanlopez42@gmail.com>
2016-10-13 18:50:37 +0200

    Update Projecte_nou.java

--- Projecte_nou/src/net/ivanlopez/Projecte_nou.java
+++ Projecte_nou/src/net/ivanlopez/Projecte_nou.java
@@ -3,7 +3,7 @@ package net.ivanlopez;
 public class Projecte_nou {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Comentari per generar el conflicte desde github
 		String hola = "hola";
 		String adeu = "adeu";
 	}
