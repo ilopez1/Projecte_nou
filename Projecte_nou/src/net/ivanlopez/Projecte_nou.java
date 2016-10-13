9fa863a438dc9aa35996ff1b46c3e8d671f8683d
ilopez1 <ivanlopez42@gmail.com>
2016-10-13 19:27:43 +0200

    Update Projecte_nou.java

--- Projecte_nou/src/net/ivanlopez/Projecte_nou.java
+++ Projecte_nou/src/net/ivanlopez/Projecte_nou.java
@@ -4,7 +4,7 @@ public class Projecte_nou {
 
 	public static void main(String[] args) {
 		
-		// Comentari per generar el conflicte desde l'IDE
+		// 
 		String hola = "hola";
 		String adeu = "adeu";
 	}
